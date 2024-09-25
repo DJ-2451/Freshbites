7e482e2de75db7ce2059d62c30dd9a5fc0b58aa0
DJ-2451 <dhanshrijagtap2412@gmail.com>
2024-09-25 15:27:40 +0530

    Headermodified

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,36 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <div className="text-lime-400">Header</div>;
+      <div class="flex items-center px-5 justify-around bg-lime-200 ">
+        <a href="/">
+          <img
+            src="https://www.graphicdesignforum.com/uploads/default/original/3X/a/f/afa4fc15043e59ef08559bdb51bf4d469af13874.png
+            "
+            class="h-24 w-49 pb-5"
+          />
+        </a>
+        <div class="flex gap-20">
+          <a class="text-green-950 font-extrabold	text-xl" href="#">
+            {" "}
+            Home
+          </a>
+          <a class="text-green-950 font-extrabold	text-xl" href="#">
+            About
+          </a>
+          <a class="text-green-950 font-extrabold	text-xl" href="#">
+            Menu
+          </a>
+          <a class="text-green-950 font-extrabold	text-xl" href="#">
+            Contact
+          </a>
+        </div>
+        <button
+          class="text-white font-semibold border px-7 py-2 bg-emerald-950 text-xl"
+          href="#"
+        >
+          Log in
+        </button>
+      </div>
     </>
   );
 };
