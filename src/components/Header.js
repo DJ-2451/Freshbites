0641c4e7a2ae11b31c7c3d02a2b6daf8582e25ec
DJ-2451 <dhanshrijagtap2412@gmail.com>
2024-09-26 21:30:26 +0530

    title and icon changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <div class="flex items-center px-5 justify-around bg-lime-200 ">
+      <div class="flex items-center px-5 justify-around  bg-lime-200 ">
         <a href="/">
           <img
             src="https://www.graphicdesignforum.com/uploads/default/original/3X/a/f/afa4fc15043e59ef08559bdb51bf4d469af13874.png
@@ -12,22 +12,33 @@ const Header = () => {
           />
         </a>
         <div class="flex gap-20">
-          <a class="text-green-950 font-extrabold	text-xl" href="#">
-            {" "}
+          <a
+            class="text-green-950 font-extrabold	text-xl hover:underline underline-offset-4"
+            href="#"
+          >
             Home
           </a>
-          <a class="text-green-950 font-extrabold	text-xl" href="#">
+          <a
+            class="text-green-950 font-extrabold	text-xl  hover:underline underline-offset-4"
+            href="#"
+          >
             About
           </a>
-          <a class="text-green-950 font-extrabold	text-xl" href="#">
+          <a
+            class="text-green-950 font-extrabold	text-xl  hover:underline underline-offset-4"
+            href="#"
+          >
             Menu
           </a>
-          <a class="text-green-950 font-extrabold	text-xl" href="#">
+          <a
+            class="text-green-950 font-extrabold	text-xl  hover:underline underline-offset-4"
+            href="#"
+          >
             Contact
           </a>
         </div>
         <button
-          class="text-white font-semibold border px-7 py-2 bg-emerald-950 text-xl hover:bg-emerald-900"
+          class="text-white font-semibold border px-7 py-2 bg-emerald-950 text-xl rounded-lg hover:bg-emerald-900"
           href="#"
         >
           Log in
